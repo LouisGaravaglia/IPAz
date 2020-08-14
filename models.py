bdde7d3da0672f724b746b3a0a27819c16af9821
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-08-13 21:50:57 -0700

    finished models for now

--- models.py
+++ models.py
@@ -11,7 +11,94 @@ def connect_db(app):
     db.app = app
     db.init_app(app)
 
-# ===========================================================   "users"   =========================================================== 
+
+# class User(db.Model):
+    
+#     __tablename__ = "users"
+    
+
+#     def __repr__(self):
+#         u=self
+#         return f"<User id={u.id} first_name={u.first_name} last_name={u.last_name} img_url={u.img_url}>"
+    
+#     id = db.Column(db.Integer,
+#                    primary_key = True,
+#                    autoincrement = True)
+#     first_name = db.Column(db.String(50),
+#                      nullable=False)
+#     last_name = db.Column(db.String(100))
+#     img_url = db.Column(db.String(100),
+#                        nullable=False)
+#     posts = db.relationship("Post", backref="user", cascade="all, delete-orphan")
+    
+    
+#     @property
+#     def full_name(self):
+#         """Return full name of user."""
+        
+#         return f"{self.first_name}{self.last_name}"        
+    
+
+# class Post(db.Model):
+    
+#     __tablename__ = "posts"
+        
+    
+#     def __repr__(self):
+#         p=self
+#         return f"<User id={p.id} title={p.title} content={p.content} created_at={p.created_at} owner_id={p.owner_id}>"
+    
+#     id = db.Column(db.Integer,
+#                    primary_key = True,
+#                    autoincrement = True)
+#     title = db.Column(db.String(50),
+#                      nullable=False)
+#     content = db.Column(db.String(1000),
+#                      nullable=False)
+#     created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
+#     owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    
+
+    
+#     @property
+#     def friendly_date(self):
+#         """Return nicely-formatted date."""
+        
+#         return self.created_at.strftime("%a %b %-d  %Y, %-I:%M %p")
+        
+    
+# class PostTag(db.Model):
+#     """Tag on a post."""
+
+#     __tablename__ = "posts_tags"
+
+#     post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), primary_key=True)
+#     tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'), primary_key=True)
+
+
+# class Tag(db.Model):
+#     """Tag that can be added to posts."""
+
+#     __tablename__ = 'tags'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.Text, nullable=False, unique=True)
+
+#     posts = db.relationship(
+#         'Post',
+#         secondary="posts_tags",
+#         backref="tags",
+#     )
+
+# ===========================================================   USER   =========================================================== 
+    
+    
+#     User
+# -
+# id PK int
+# username string
+# password string
+# bio string
 
 class User(db.Model):
     
@@ -22,14 +109,13 @@ class User(db.Model):
         u=self
         return f"<User id={u.id} first_name={u.first_name} last_name={u.last_name} img_url={u.img_url}>"
     
-    id = db.Column(db.Integer,
-                   primary_key = True,
-                   autoincrement = True)
-    first_name = db.Column(db.String(50),
-                     nullable=False)
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(100))
-    img_url = db.Column(db.String(100),
-                       nullable=False)
+    username = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+    bio = db.Column(db.String(2000), nullable=False)
+    img_url = db.Column(db.String(100), nullable=False)
     posts = db.relationship("Post", backref="user", cascade="all, delete-orphan")
     
     
@@ -37,89 +123,9 @@ class User(db.Model):
     def full_name(self):
         """Return full name of user."""
         
-        return f"{self.first_name}{self.last_name}"        
-    
-# ===========================================================   "posts"   =========================================================== 
+        return f"{self.first_name}{self.last_name}"  
 
-class Post(db.Model):
-    
-    __tablename__ = "posts"
-        
-    
-    def __repr__(self):
-        p=self
-        return f"<User id={p.id} title={p.title} content={p.content} created_at={p.created_at} owner_id={p.owner_id}>"
-    
-    id = db.Column(db.Integer,
-                   primary_key = True,
-                   autoincrement = True)
-    title = db.Column(db.String(50),
-                     nullable=False)
-    content = db.Column(db.String(1000),
-                     nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
-    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    
-
-    
-    @property
-    def friendly_date(self):
-        """Return nicely-formatted date."""
-        
-        return self.created_at.strftime("%a %b %-d  %Y, %-I:%M %p")
-        
-# ===========================================================   "posts_tags"   =========================================================== 
-    
-class PostTag(db.Model):
-    """Tag on a post."""
-
-    __tablename__ = "posts_tags"
-
-    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), primary_key=True)
-    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'), primary_key=True)
-
-# ===========================================================   "tags"   =========================================================== 
-
-class Tag(db.Model):
-    """Tag that can be added to posts."""
-
-    __tablename__ = 'tags'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Text, nullable=False, unique=True)
-
-    posts = db.relationship(
-        'Post',
-        secondary="posts_tags",
-        backref="tags",
-    )
-    
-    
-#     User
-# -
-# id PK int
-# username string
-# password string
-# bio string
-
-# Wine
-# -
-# id PK int
-# winery int
-# country string
-# vintage string
-# varietal string
-# wine_style string
-# name string
-# area string
-# description string
-# rating float
-
-# Saved
-# -
-# id PK int
-# wine_id int FK >- Wine.id
-# user_id int FK >- User.id
+# ===========================================================   POST   =========================================================== 
 
 # Review
 # -
@@ -146,9 +152,70 @@ class Post(db.Model):
     review = db.Column(db.String(5000),
                      nullable=False)
     wine_id = db.Column(db.Integer, db.ForeignKey('wines.id'), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)    
+
+# ===========================================================   WINE   =========================================================== 
+    
+# Wine
+# -
+# id PK int
+# winery int
+# country string
+# vintage string
+# varietal string
+# wine_style string
+# name string
+# area string
+# description string
+# rating float
+
+class Wine(db.Model):
+    
+    __tablename__ = "wines"
+    
+
+    def __repr__(self):
+        u=self
+        return f"<User id={u.id} first_name={u.first_name} last_name={u.last_name} img_url={u.img_url}>"
+    
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    winery = db.Column(db.String(50), nullable=False)
+    country = db.Column(db.String(100))
+    vintage = db.Column(db.String(100))
+    varietal = db.Column(db.String(100))
+    wine_style = db.Column(db.String(2000), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    area = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(100), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    fav_users = db.relationship('User', secondary="favorites", backref="fav_wines")
     
     
+    @property
+    def full_name(self):
+        """Return full name of user."""
+        
+        return f"{self.first_name}{self.last_name}"  
+
+# ===========================================================   FAVORITES   =========================================================== 
+
+# Saved
+# -
+# id PK int
+# wine_id int FK >- Wine.id
+# user_id int FK >- User.id
+
+class Favorite(db.Model):
+    
+    __tablename__ = "favorites"
+    
+
+    wine_id = db.Column(db.Integer, db.ForeignKey('wines.id'), primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+
+
+    
+# ===========================================================   TAGS   ===========================================================    
 
 # Eye
 # -
@@ -170,15 +237,47 @@ class EyeTag(db.Model):
 # id PK int 
 # name string
 
+class NoseTag(db.Model):
+    """Tag that can be added to posts."""
+
+    __tablename__ = 'nose_tags'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False, unique=True)
+    posts = db.relationship('Post', secondary="posts_nose_tags", backref="nose_tags")
+    wines = db.relationship('Wine', secondary="wines_nose_tags", backref="nose_tags")
+
 # Mouth
 # -
 # id PK int
 # name string
 
+class MouthTag(db.Model):
+    """Tag that can be added to posts."""
+
+    __tablename__ = 'mouth_tags'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False, unique=True)
+    posts = db.relationship('Post', secondary="posts_mouth_tags", backref="mouth_tags")
+    wines = db.relationship('Wine', secondary="wines_mouth_tags", backref="mouth_tags")
+
 # Finish
 # -
 # id PK int 
 # name string
+
+class FinishTag(db.Model):
+    """Tag that can be added to posts."""
+
+    __tablename__ = 'finish_tags'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False, unique=True)
+    posts = db.relationship('Post', secondary="posts_finish_tags", backref="finish_tags")
+    wines = db.relationship('Wine', secondary="wines_finish_tags", backref="finish_tags")
+
+# ===========================================================   POST TAGS   =========================================================== 
 
 # PostEyeTag
 # -
@@ -199,15 +298,42 @@ class PostEyeTag(db.Model):
 # post_id int FK >- Review.id
 # tag_id int FK >- Nose.id
 
+class PostNoseTag(db.Model):
+    """Tag on a post."""
+
+    __tablename__ = "posts_nose_tags"
+
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey('nose_tags.id'), primary_key=True)
+
 # PostMouthTag
 # -
 # post_id int FK >- Review.id
 # tag_id int FK >- Mouth.id
 
+class PostMoutbTag(db.Model):
+    """Tag on a post."""
+
+    __tablename__ = "posts_mouth_tags"
+
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey('mouth_tags.id'), primary_key=True)
+
 # PostFinishTag
 # -
 # post_id int FK >- Review.id
 # tag_id int FK >- Finish.id
+
+class PostFinishTag(db.Model):
+    """Tag on a post."""
+
+    __tablename__ = "posts_finish_tags"
+
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey('finish_tags.id'), primary_key=True)
+
+# ===========================================================   WINE TAGS   =========================================================== 
+
 
 # WineEyeTag
 # -
@@ -227,15 +353,39 @@ class WineEyeTag(db.Model):
 # wine_id int 
 # tag_id int 
 
+class WineEyeTag(db.Model):
+    """Tag on a post."""
+
+    __tablename__ = "wines_nose_tags"
+
+    wine_id = db.Column(db.Integer, db.ForeignKey('wines.id'), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey('nose_tags.id'), primary_key=True)
+
 # WineMouthTag
 # -
 # wine_id int 
 # tag_id int 
 
+class WineMouthTag(db.Model):
+    """Tag on a post."""
+
+    __tablename__ = "wines_mouth_tags"
+
+    wine_id = db.Column(db.Integer, db.ForeignKey('wines.id'), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey('mouth_tags.id'), primary_key=True)
+
 # WineFinishTag
 # -
 # wine_id int
 # tag_id int
+
+class WineFinishTag(db.Model):
+    """Tag on a post."""
+
+    __tablename__ = "wines_finish_tags"
+
+    wine_id = db.Column(db.Integer, db.ForeignKey('wines.id'), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey('finish_tags.id'), primary_key=True)
 
 
    # ===========================================================   notes   =========================================================== 
