062c94b56e5102093824e30e2fae25de9866ec99
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-08-18 10:00:00 -0700

    added a makeAPIcall() function in js file to make it easier to get all wines down for testing

--- static/script.js
+++ static/script.js
@@ -1,3 +1,5 @@
+
+
 // This function is here so that the varietal selects do not get added upon each other when user hits the back GamepadButton.
 window.addEventListener( "pageshow", function ( event ) {
   var historyTraversal = event.persisted || 
@@ -14,6 +16,7 @@ $("#varietals").on("click", ".varietals", async function(e) {
   const selected_button = e.target;
   selected_button.classList.toggle("is-focused")
   varietal = selected_button.innerText
+  console.log("clickng");
   await sendVarietals(varietal)
 })
 
@@ -21,6 +24,13 @@ $("#varietals").on("click", ".varietals", async function(e) {
 async function sendVarietals(varietal) {
   const res = await axios.get(`/show_combined_question/${varietal}`)
  
+}
+
+async function makeAPIcall() {
+  await axios.get("/api/get_red_wines")
+  await axios.get("/api/get_white_wines")
+  await axios.get("/api/get_rose_wines")
+
 }
 
 
