6173e2c49929dd7f221adbead902254c90086053
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-08-20 15:12:08 -0700

    refactored Models.py

--- models.py
+++ models.py
@@ -12,102 +12,16 @@ def connect_db(app):
     db.init_app(app)
 
 
-# class User(db.Model):
-    
-#     __tablename__ = "users"
-    
-
-#     def __repr__(self):
-#         u=self
-#         return f"<User id={u.id} first_name={u.first_name} last_name={u.last_name} img_url={u.img_url}>"
-    
-#     id = db.Column(db.Integer,
-#                    primary_key = True,
-#                    autoincrement = True)
-#     first_name = db.Column(db.String(50),
-#                      nullable=False)
-#     last_name = db.Column(db.String(100))
-#     img_url = db.Column(db.String(100),
-#                        nullable=False)
-#     posts = db.relationship("Post", backref="user", cascade="all, delete-orphan")
-    
-    
-#     @property
-#     def full_name(self):
-#         """Return full name of user."""
-        
-#         return f"{self.first_name}{self.last_name}"        
-    
-
-# class Post(db.Model):
-    
-#     __tablename__ = "posts"
-        
-    
-#     def __repr__(self):
-#         p=self
-#         return f"<User id={p.id} title={p.title} content={p.content} created_at={p.created_at} owner_id={p.owner_id}>"
-    
-#     id = db.Column(db.Integer,
-#                    primary_key = True,
-#                    autoincrement = True)
-#     title = db.Column(db.String(50),
-#                      nullable=False)
-#     content = db.Column(db.String(1000),
-#                      nullable=False)
-#     created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
-#     owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    
-
-    
-#     @property
-#     def friendly_date(self):
-#         """Return nicely-formatted date."""
-        
-#         return self.created_at.strftime("%a %b %-d  %Y, %-I:%M %p")
-        
-    
-# class PostTag(db.Model):
-#     """Tag on a post."""
-
-#     __tablename__ = "posts_tags"
-
-#     post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), primary_key=True)
-#     tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'), primary_key=True)
-
-
-# class Tag(db.Model):
-#     """Tag that can be added to posts."""
-
-#     __tablename__ = 'tags'
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.Text, nullable=False, unique=True)
-
-#     posts = db.relationship(
-#         'Post',
-#         secondary="posts_tags",
-#         backref="tags",
-#     )
-
 # ===========================================================   USER   =========================================================== 
     
     
-#     User
-# -
-# id PK int
-# username string
-# password string
-# bio string
-
 class User(db.Model):
     
-    __tablename__ = "users"
-    
+    __tablename__ = "users" 
 
     def __repr__(self):
         u=self
-        return f"<User id={u.id} first_name={u.first_name} last_name={u.last_name} img_url={u.img_url}>"
+        return f"<User id={u.id} name={u.name} username={u.username} bio={u.bio} img_url={u.img_url}>"
     
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     name = db.Column(db.String(50), nullable=False)
@@ -116,12 +30,6 @@ class User(db.Model):
     bio = db.Column(db.String(2000), nullable=False)
     img_url = db.Column(db.String(100), nullable=False)
     posts = db.relationship("Post", backref="user", cascade="all, delete-orphan")
-    
-    @property
-    def full_name(self):
-        """Return full name of user."""
-        
-        return f"{self.first_name}{self.last_name}"  
     
     
     @classmethod
@@ -167,13 +75,6 @@ class User(db.Model):
 
 # ===========================================================   POST   =========================================================== 
 
-# Review
-# -
-# id PK int
-# wine_id int FK >- Wine.id
-# rating float
-# review string
-# user_id int FK >- User.id
 
 class Post(db.Model):
     
@@ -196,18 +97,6 @@ class Post(db.Model):
 
 # ===========================================================   WINE   =========================================================== 
     
-# Wine
-# -
-# id PK int
-# winery int
-# country string
-# vintage string
-# varietal string
-# wine_style string
-# name string
-# area string
-# description string
-# rating float
 
 class Wine(db.Model):
     
@@ -242,16 +131,9 @@ class Wine(db.Model):
             'rating': self.rating
         }
     
-    
-    
 
 # ===========================================================   FAVORITES   =========================================================== 
 
-# Saved
-# -
-# id PK int
-# wine_id int FK >- Wine.id
-# user_id int FK >- User.id
 
 class Favorite(db.Model):
     
@@ -266,15 +148,6 @@ class Favorite(db.Model):
 
    # ===========================================================   notes   =========================================================== 
  
-      
-    # def greet(self):
-    #     return f"Hi, my name is {self.name} and i'm a {self.species}!"
-    
-    # def feed(self, amt=20):
-    #     """Update hunger based off of amt"""
-        
-    #     self.hunger -= amt
-    #     self.hunger = max(self.hunger, 0)
     
     # to run this class use the following
     # ipython3
